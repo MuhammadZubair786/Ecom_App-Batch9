b784bd86b4c0aa253a75719b2d2387da7c34dfae
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2026-08-15 20:08:27 +0500

    Add files via upload

--- signUp.js
+++ signUp.js
@@ -4,27 +4,67 @@ var password = document.getElementById("password")
 var username = document.getElementById("name")
 
 
-signup.addEventListener("click", async function(){
-    console.log(email.value,password.value) 
+signup.addEventListener("click", async function () {
+    console.log(email.value, password.value)
 
-   await firebase.auth().createUserWithEmailAndPassword(email.value,password.value)
-    .then((result)=>{
-        console.log(result.user.uid) // database id 
-        alert("SignUp Successfully")
-        firebase.database().ref("user").child(result.user.uid).set({
-            email:email.value,
-            password:password.value,
-            name:username.value
+    await firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+        .then((result) => {
+            console.log(result.user.uid) // database id 
+            alert("SignUp Successfully")
+            firebase.database().ref("user").child(result.user.uid).set({
+                email: email.value,
+                password: password.value,
+                name: username.value
+            })
+            setTimeout(() => {
+                // window.location.href="login.html"
+                window.location.replace("login.html")
+            }, 2000)
         })
-        setTimeout(()=>{
-            // window.location.href="login.html"
-            window.location.replace("login.html")
-        },2000)
-    })
-    .catch((error)=>{ 
-        alert(error)
+        .catch((error) => {
+            alert(error)
 
-    })
+        })
 
 
 })
+
+
+async function uploadimage() {
+    const fileInput = document.getElementById("imageUpload")
+    const file = fileInput.files[0]
+    if (!file) {
+      return  alert("please select profile image")
+    }
+    const cloudName = ""
+    const uploadFolder = ""
+    const formData = new FormData()
+    formData.append("file",file)
+    formData.append("upload_preset",uploadFolder)
+    var loading = document.getElementById("loading")
+    var btn = document.getElementById("btn")
+    btn.style.display="none"
+    loading.style.display="inline"
+
+    try{
+        const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,{
+            method:"POST",
+            body:formData
+        })
+
+        const data =await response.json()
+
+        console.log(data)
+        alert("profile imaeg upload")
+        window.location.href=data.secure_url
+
+
+
+
+    }
+    catch(e){
+        alert("error",e)
+    }
+
+
+}
